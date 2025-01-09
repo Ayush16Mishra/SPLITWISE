31a20f3ab6ab5f34a8ccee7e715f2a00162e2643
Ayush16Mishra <Ayushpc025@gmail.com>
2025-01-09 10:29:47 +0530

    Replaced bcrypt with bcryptjs to fix deployment issues

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 // auth.js
 
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 const client = require('../db'); // Import the client from db.js
